59f697e17444335098425117ba28de62ccd1b126
David Rogers <davidrogers@Davids-MacBook-Pro.local>
2019-12-18 10:29:14 -0800

    Hotjar tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,14 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-hotjar`,
+      options: {
+        id: 1235021,
+        sv: 6
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
