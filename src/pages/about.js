4f6eb0db29d9be1a0fd99056e0f80357e945e598
David Rogers <davidrogers@Davids-MacBook-Pro.local>
2020-03-04 03:00:45 -0800

    Netlify debugging

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,10 +4,6 @@ import Layout from "../components/layout"
 import Nav from "..//components/nav"
 import Footer from "..//components/footer"
 
-import Project from "..//components/project"
-import MEMS from "../images/mems.jpg"
-import GAMMA from "../images/mems.jpg"
-
 const About = () => (
   <Layout>
     <SEO title="About" />
